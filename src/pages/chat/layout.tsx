b56244d2cd05e9c61706de8ddca687d498c70e70
Johan Montorfano <me@johanmontorfano.com>
2025-05-16 21:36:42 +0200

    add: chat name change

--- src/pages/chat/layout.tsx
+++ src/pages/chat/layout.tsx
@@ -1,9 +1,25 @@
 import { RouteSectionProps, useParams } from "@solidjs/router";
-import { createEffect, createSignal, For, Show } from "solid-js";
-import { chats } from "../../p2p/local";
+import { createEffect, createSignal, For, Index, Show } from "solid-js";
+import { Popup } from "../../component/popup";
+import { saveChat } from "../../p2p/chats";
+import { chats, setChats } from "../../p2p/local";
+
+/// A separate component for chat entries allows to not loose reactivity
+/// when dealing with changes in the chat name/last chat.
+function ChatEntry(props: {id: number}) {
+    const params = useParams();
+
+    return <a href={`/chat/${chats()[props.id].peerId}`} 
+        class="border-b w-full h-[36px] flex items-center"
+        data-active={params.id === chats()[props.id].peerId}
+    >
+        {chats()[props.id].profileLocalName}
+    </a>
+}
 
 export function ChatLayout(props: RouteSectionProps<unknown>) {
     const [chatI, setChatI] = createSignal(-1);
+    const [profileOpen, setProfileOpen] = createSignal(false);
     const params = useParams();
 
     createEffect(() => {
@@ -15,25 +31,35 @@ export function ChatLayout(props: RouteSectionProps<unknown>) {
     return <div class="grid grid-cols-[1fr_4fr] w-full h-full">
         <div class="border-r w-full flex flex-col">
             <a href="/chat/new">New chat</a>
-            <For each={chats()}>{chat => 
-                <a href={`/chat/${chat.peerId}`} 
-                    class="border-b w-full h-[36px] flex items-center"
-                    data-active={params.id === chat.peerId}
-                >
-                    {chat.profileLocalName}
-                </a>
-            }
-            </For>
+            <Index each={chats()}>{(_, i) => <ChatEntry id={i} />}</Index>
         </div>
         <div>
             <div class="flex w-full h-[4dvh] border-b justify-center items-center">
                 <Show when={chatI() > -1} fallback="New chat">
-                    <p class="cursor-pointer">
+                    <p class="cursor-pointer"
+                        onClick={() => setProfileOpen(true)}
+                    >
                         {chats()[chatI()].profileLocalName}
                     </p>
                 </Show>
             </div>
             {props.children}
         </div>
+        <Popup show={profileOpen()} onClose={() => setProfileOpen(false)}>
+            <h1 class="text-2xl">
+                {chats()[chatI()].profileLocalName}
+            </h1>
+            <br />
+            <p>Peer identifier: {chats()[chatI()].peerId}</p>
+            <label>Name: </label>
+            <input type="text"
+                value={chats()[chatI()].profileLocalName}
+                onChange={ev => setChats(p => {
+                    p[chatI()].profileLocalName = ev.target.value;
+                    saveChat(p[chatI()]);
+                    return p;
+                })}
+            />
+        </Popup>
     </div>
 }
