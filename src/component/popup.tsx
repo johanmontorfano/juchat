ceae590f02182d811eacf53a38c18160ee706244
Johan Montorfano <me@johanmontorfano.com>
2025-05-16 21:18:18 +0200

    add: popups

---
+++ src/component/popup.tsx
@@ -0,0 +1,27 @@
+import { JSX, Show } from "solid-js";
+import { Portal } from "solid-js/web";
+import { IoCloseOutline } from "solid-icons/io";
+
+export function Popup(
+    props: {
+        show: boolean,
+        onClose: () => void,
+        children: JSX.Element
+    }
+) {
+    return <Show when={props.show}>
+        <Portal>
+            <div class="popup-container">
+                <div>
+                    <div class="w-full flex justify-end">
+                        <IoCloseOutline onClick={props.onClose}
+                            size={32}
+                            cursor="pointer"
+                        />
+                    </div>
+                    {props.children}
+                </div>
+            </div>
+        </Portal>
+    </Show>
+}
