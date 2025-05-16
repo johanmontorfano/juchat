0199826f74c31e6295f33589c46236a0ace2845c
Johan Montorfano <me@johanmontorfano.com>
2025-05-16 20:23:54 +0200

    add: coloring for active chat

--- src/pages/chat/layout.tsx
+++ src/pages/chat/layout.tsx
@@ -1,24 +1,37 @@
 import { RouteSectionProps, useParams } from "@solidjs/router";
-import { For } from "solid-js";
+import { createEffect, createSignal, For, Show } from "solid-js";
 import { chats } from "../../p2p/local";
 
 export function ChatLayout(props: RouteSectionProps<unknown>) {
+    const [chatI, setChatI] = createSignal(-1);
     const params = useParams();
 
+    createEffect(() => {
+        const i = chats().findIndex(v => v.peerId === params.id);
+
+        setChatI(i);
+    });
+
     return <div class="grid grid-cols-[1fr_4fr] w-full h-full">
-        <div class="border-r">
+        <div class="border-r w-full flex flex-col">
             <a href="/chat/new">New chat</a>
-            <div>
-                <For each={chats()}>
-                    {chat => <a href={`/chat/${chat.peerId}`}>
-                        {chat.profileLocalName}
-                    </a>}
-                </For>
-            </div>
+            <For each={chats()}>{chat => 
+                <a href={`/chat/${chat.peerId}`} 
+                    class="border-b w-full h-[36px] flex items-center"
+                    data-active={params.id === chat.peerId}
+                >
+                    {chat.profileLocalName}
+                </a>
+            }
+            </For>
         </div>
         <div>
-            <div class="w-full h-[4dvh] border-b align-center">
-                {params.id}
+            <div class="flex w-full h-[4dvh] border-b justify-center items-center">
+                <Show when={chatI() > -1} fallback="New chat">
+                    <p class="cursor-pointer">
+                        {chats()[chatI()].profileLocalName}
+                    </p>
+                </Show>
             </div>
             {props.children}
         </div>
