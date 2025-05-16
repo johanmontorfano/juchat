6edfe585bc713ed4f4303bddce529c161b502972
Johan Montorfano <me@johanmontorfano.com>
2025-05-17 01:35:36 +0200

    add: grid-cols-* to safelist

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,17 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    safelist: [
+        "grid-cols-[1fr_4fr]",
+        "grid-cols-[2fr_4fr]",
+        "grid-cols-[2fr_3fr]",
+        "grid-cols-[1fr]"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 };
 
 export default config;
