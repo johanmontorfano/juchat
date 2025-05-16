b0e344d27cca935c48b7863c84b389713803a92c
Johan Montorfano <me@johanmontorfano.com>
2025-05-16 22:42:21 +0200

    add: bubbles

--- src/pages/chat/[id].tsx
+++ src/pages/chat/[id].tsx
@@ -3,6 +3,17 @@ import { createSignal, For } from "solid-js";
 import { chatEvent, saveChat } from "../../p2p/chats";
 import { chats, onConnection, peer, setChats } from "../../p2p/local";
 
+function Bubble(props: { from: "local" | "remote", content: string }) {
+    const local = props.from === "local";
+    const bubble = local ? "bg-blue-500 text-white" : "bg-slate-200";
+
+    return <div class={`flex ${local ? "justify-end" : ""} w-full`}>
+        <p class={`p-[4px] m-[4px] min-w-[80px] ${bubble} rounded-[5px]`}>
+            {props.content}
+        </p>
+    </div>
+}
+
 export function Chat() {
     const [content, setContent] = createSignal("");
     const params = useParams();
@@ -26,9 +37,7 @@ export function Chat() {
 
     return <div>
         <div>
-            <For each={chats()[i].chatHistory}>
-                {data => <p>{data.from}: {data.content}</p>}
-            </For>
+            <For each={chats()[i].chatHistory} children={Bubble} />
         </div>
         <div>
             <form onSubmit={onSubmit}>
