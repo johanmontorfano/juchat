3e46107c32f172a4fd0838fb02a140673eb61c51
Johan Montorfano <me@johanmontorfano.com>
2025-05-17 13:55:12 +0200

    rm: id from the top bar

--- src/layout.tsx
+++ src/layout.tsx
@@ -5,7 +5,6 @@ export function RootLayout(props: RouteSectionProps<unknown>) {
     return <div class="h-dvh bg-white dark:bg-slate-950 dark:text-white">
         <div class="flex items-center border-b h-[6dvh]">
             <img src="/assets/logo.png" width={64} height={64} />
-            <p>{peer.id}</p>
         </div>
         <div class="h-[94dvh]">
             {props.children}
