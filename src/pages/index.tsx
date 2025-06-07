174ba129ddd15fa1538c756d38645a313344c0fb
Johan Montorfano <me@johanmontorfano.com>
2025-06-07 14:38:49 +0200

    add: better index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,18 @@
 export function Index() {
-    return <div>
-        <p>Hi</p>
-        <a href="/chat">Go chat</a>
+    return <div class="w-full flex justify-center">
+        <div class="w-full max-w-[1000px] p-4">
+            <h1 class="font-bold text-4xl">Welcome to Ju Chat</h1>
+            <br />
+            <div>
+                <ul class="list-disc">
+                    <li>No centralized storage and communication</li>
+                    <li>Zero-trust infrastructure</li>
+                    <li>Local-first infrastructure</li>
+                    <li>Rich-text messaging</li>
+                    <li>No account needed</li>
+                    <li>Full privacy and anonymity</li>
+                </ul>
+            </div>
+        </div>
     </div>
 }
